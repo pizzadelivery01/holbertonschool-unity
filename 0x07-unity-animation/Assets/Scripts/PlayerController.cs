b0b687b603742e1195f18faa5ac564047fcc9b1a
pizzadelivery01 <50601948+pizzadelivery01@users.noreply.github.com>
2020-10-07 21:24:00 -0400

    update

--- 0x07-unity-animation/Assets/Scripts/PlayerController.cs
+++ 0x07-unity-animation/Assets/Scripts/PlayerController.cs
@@ -15,16 +15,18 @@ public class PlayerController : MonoBehaviour
     private float jumpHeight = 3.5f;
     private float gravity = -11.81f;
     private Vector3 velocity;
+	Animator ani;
 
     /// <summary>
     /// Initializes components and spawn position.
     /// </summary>
     void Start()
     {
+		ani = transform.GetChild(0).GetComponent<Animator>();
         controller = GetComponent<CharacterController>();
         playerTransform = GetComponent<Transform>();
         spawnPosition = playerTransform.position;
-        spawnPosition.y += 20;
+        spawnPosition.y += 30;
     }
 
     /// <summary>
@@ -34,11 +36,22 @@ public class PlayerController : MonoBehaviour
     {
         if (controller.isGrounded && velocity.y < 0)
         {
+			ani.SetBool("Falling", false);
             velocity.y = -0.01f;
         }
 
+
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
+
+		if (controller.isGrounded && (Input.GetButton("Horizontal") || Input.GetButton("Vertical")))
+		{
+			ani.SetBool("Running", true);
+		}
+		else
+		{
+			ani.SetBool("Running", false);
+		}
 
         Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
         Vector3 force = Vector3.zero;
@@ -53,13 +66,18 @@ public class PlayerController : MonoBehaviour
         if (controller.isGrounded && Input.GetButton("Jump"))
         {
             velocity.y += Mathf.Sqrt(jumpHeight * -2f * gravity);
+			ani.SetTrigger("Jump");
         }
         velocity.y += gravity * Time.deltaTime;
         controller.Move(velocity * Time.deltaTime + force);
 
-        if (playerTransform.position.y < -20)
+        if (playerTransform.position.y < -10)
         {
-            playerTransform.position = spawnPosition;
+			ani.SetBool("Falling", true);
+			if (playerTransform.position.y < -65)
+			{
+				playerTransform.position = spawnPosition;
+			}
         }
     }
 }
