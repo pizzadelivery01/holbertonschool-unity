9ed9e4ee6d1f3555e8bd6424c7659d3188431bce
pizzadelivery01 <50601948+pizzadelivery01@users.noreply.github.com>
2020-10-21 23:58:45 -0400

    update

---
+++ 0x08-unity-audio/Assets/Scripts/RunningFootsteps.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class RunningFootsteps : MonoBehaviour
+{
+	public AudioClip clip;
+    public AudioSource audioSource;
+
+    private void Step()
+    {
+        audioSource.PlayOneShot(clip);
+    }
+}
